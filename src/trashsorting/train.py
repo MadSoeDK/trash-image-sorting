d696878fbb0c1463be8ed646a07cef719d80a3eb
Vaksth <s234855@dtu.dk>
2026-01-20 16:41:51 +0100

    PyTorch profiling train.py on CPU with no optimizations yet. Profiling is done on max 50 batches of size 32 in 1 epoch.

--- src/trashsorting/train.py
+++ src/trashsorting/train.py
@@ -2,6 +2,8 @@ from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
 from torch.utils.data import DataLoader
+from pytorch_lightning.profilers import PyTorchProfiler
+import torch
 
 import yaml
 from trashsorting.data import TrashDataPreprocessed
@@ -43,10 +45,35 @@ def train(
         wandb_logger = WandbLogger(project=os.getenv("WANDB_PROJECT"))
         wandb_logger.experiment.config["batch_size"] = batch_size
 
+
+    
+    
+    profiler = PyTorchProfiler(
+        schedule=torch.profiler.schedule(
+            wait=1,
+            warmup=1,
+            active=10,      # profile 10 steps
+            repeat=1
+        ),
+        activities=[
+            torch.profiler.ProfilerActivity.CPU,
+        ],
+        record_shapes=True,
+        profile_memory=True,
+        with_stack=True,   # critical for knowing WHERE time is spent
+        with_flops=True,
+        on_trace_ready=torch.profiler.tensorboard_trace_handler("./tb_logs")
+    )
+
+
+
+
     trainer = Trainer(
         max_epochs=max_epochs,
         callbacks=[checkpoint_callback],
-        logger=wandb_logger
+        logger=wandb_logger,
+        profiler=profiler,
+        limit_train_batches=50
     )
 
     trainer.fit(
